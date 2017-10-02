5dbc0def9b131fdac52fde94c1e7adafe9d2ce59
nishant <nishant691989@gmail.com>
2017-10-02 12:19:17 -0400

    Step1

---
+++ Desktop/Steps/step2/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import {Component} from '@angular/core';
+import {DataService} from "../services/DataService"
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html'
+})
+//Displaying JSON data and filtering with name, category and amount
+export class HomeComponent {
+  dataset: Array<any>;
+  isOrder: boolean = false;
+  column: string = 'name';
+  direction: number;
+
+  constructor(private DataService: DataService) {
+
+  }
+
+  //subscribing to data and display on Initial state.
+  ngOnInit() {
+    this.DataService.GetData().subscribe(data => {
+      this.dataset = data;
+      console.log(this.dataset);
+    })
+  
+  }
+
+  
+}
---
+++ Desktop/Steps/step2/src/app/services/DataService.ts
@@ -0,0 +1,21 @@
+import {Injectable} from "@angular/core"
+import {Observable} from "rxjs/Observable"
+import {Http, Response} from "@angular/http"
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class DataService {
+  constructor (private http:Http) {
+
+  }
+  GetData() : Observable<any> {
+    console.log(this.http.get("../data.json")
+      .map((response: Response) => {
+        return <any>response.json();
+      }));
+    return this.http.get("../data.json")
+      .map((response: Response) => {
+        return <any>response.json();
+      })
+  }
+}
